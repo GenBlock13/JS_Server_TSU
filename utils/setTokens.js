13c1795f9425ea674916bd3365e1af6d412066e0
Reptilius <blockgen@yandex.ru>
2024-05-15 10:51:47 +0300

    tokenDto

--- utils/setTokens.js
+++ utils/setTokens.js
@@ -3,7 +3,7 @@ import { UserDto } from '../dtos/UserDto.js'
 
 export default async (user) => {
     const userDto = new UserDto(user)
-    const tokens = tokenService.generateTokens({...UserDto})
+    const tokens = tokenService.generateTokens({...userDto})
     await tokenService.saveToken(userDto.id, tokens.refreshToken)
     return {
         ...tokens,
