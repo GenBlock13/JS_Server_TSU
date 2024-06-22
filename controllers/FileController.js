69e6c7a483f2575a3df9ac67992867ea6c453626
Reptilius <blockgen@yandex.ru>
2024-06-22 18:32:03 +0300

    last_vers

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -2,36 +2,36 @@ import { ApiError } from '../exceptions/ApiError.js'
 import { fileService } from '../services/FileService.js'
 
 class FileController {
-    // метод для загрузки одной фотографии
     uploadImage(req, res) {
         try {
-            const img = req.file ? req.file.filename : ''
-            if (!req.file) {
-                const imgUrl = img ? fileService.uploadImage(img) : ''
-           const url = { img : imgUrl }
+           const img = req.file ? req.file.filename : ''
+           if (!req.file) {
+            const imgUrl = img ? fileService.uploadImage(img) : ''
+            const url = { img: imgUrl }
+            return res.json(url)
+           }
+           const url = fileService.uploadImage(img)
            return res.json(url)
-            }
         } catch (e) {
            throw ApiError.badRequest('Ошибка в загрузке файла', e) 
         }
     }
     
-    // метод для загрузки нескольких фотографии
     uploadImages(req, res) {
         try {
             const thumb = req.files.thumbnail
-           ? req.files.thumbnail[0]
-           : ''
+              ? req.files.thumbnail[0]
+              : ''
             const gallery = req.files.gallery ? req.files.gallery : []
             if (!req.files.thumbnail || !req.files.gallery) {
-                const urlThumb = thumb
-                ? fileService.uploadImage(req.files.thumbnail[0].filename)
-                : ''
-            const urlsGallery = gallery.length > 0 ? fileService.uploadMultiImg(gallery): undefined
-            const urls = { thumbUrl: urlTumb.url, galleryUrl: urlsGallery }
+                const urlTumb = thumb
+                  ? fileService.uploadImage(req.files.thumbnail[0].filename)
+                  : ''
+                const urlsGallery = gallery.length > 0 ? fileService.uploadMultiImg(gallery) : undefined
+                const urls = { thumbUrl: urlThumb.url, galleryUrl: urlsGallery }
+                return res.json(urls)
             }
-
-
+            const urls = fileService.uploadImages(thumb, gallery)
             return res.json(urls)
         } catch (e) {
             throw ApiError.badRequest('Ошибка в загрузке файлов', e)
--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,15 +1,11 @@
 import { ApiError } from '../exceptions/ApiError.js'
 
-export default function(err, req, res, next){
-
-    console.log(err)
-
-    if(err instanceof ApiError){
-
+export default function(err, req, res, next) {
+    if(err instanceof ApiError) {
         return res.status(err.status).json({message: err.message, errors: err.errors})
-
     }
-
-    return res.status(500).json({message: 'Unexpected Server error occured'})
-
+    if (err.name === 'SequelizeUniqueConstraintError') {
+        return res.status(500).json({message: 'Заголовок должен быть уникальным'})
+    }
+    return res.status(500).json({message: 'Произошла непредвиденная ошибка сервера', errors: err})
 }
